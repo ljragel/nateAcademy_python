aa0e7bf9b67a7fe5c5b0bd356b70a63ceeaeb048
sanchezfdezjavier <32944505+sanchezfdezjavier@users.noreply.github.com>
2018-03-04 03:13:57 +0100

    Introducido el archivo "temperature_conversor"

---
+++ temperature_conversor.py
@@ -0,0 +1,4 @@
+print("Bienvenido, introduce los grados que queies convertir")
+
+raw_grados=float(input(" "))
+print("{} farenhait, ".format(raw_grados) + "son {} grados celsius".format((raw_grados -32)/1.8))
