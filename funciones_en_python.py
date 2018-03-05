3e7408a13d743f00052f578d16034cad0d0f433b
sanchezfdezjavier <32944505+sanchezfdezjavier@users.noreply.github.com>
2018-03-05 02:30:54 +0100

    Added "buit_in_python" and "fuinciones_en_python" files.

---
+++ funciones_en_python.py
@@ -0,0 +1,15 @@
+
+def nombre_funcion(argumento):
+    #cuerpo de la funcion, en este medidor de logitud de lista
+    length=0
+    for item in argumento:
+        length+=1
+    return length
+
+my_list=[1,2,3,4,5,6,7,8,9,10]
+
+print(nombre_funcion(my_list))
+
+#esto es un ejemplo delo que hace la funcion"len()" en python, equivalente a lo que hace en java .length()
+
+
