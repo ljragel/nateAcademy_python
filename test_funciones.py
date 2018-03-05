27544eb2b9981b4e555e1ac25c7e55c505c7b596
sanchezfdezjavier <32944505+sanchezfdezjavier@users.noreply.github.com>
2018-03-05 03:11:32 +0100

    Added "test_funciones", an easy function implementation example in python.

---
+++ test_funciones.py
@@ -0,0 +1,11 @@
+
+def quita_impares(lista):
+    result=[]
+    for number in example_list:
+        if number%2==0:
+            result.append(number)
+    return result
+
+example_list=[0,1,2,3,4,5,6,7,8,9]
+
+print(quita_impares(example_list))
